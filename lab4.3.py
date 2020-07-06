d46c0adbd53e293fa153c296ca8d6c6ab44292b4
pavloova <67763277+pavloova@users.noreply.github.com>
2020-07-06 09:59:41 +0300

    Update lab4.3.py

--- lab4.3.py
+++ lab4.3.py
@@ -1,8 +1,8 @@
 def mechenergy(m=0,h=0,v=0): 
     """функция вычисляет полную механическую энергию тела по входным параметрам: масса тела, высота и скорость полета"""
     from physicalconstants import g
-    full_mech_energy=(m*(v**2)/2) + m*h*g
-    return(full_mech_energy)
+    fme=(m*(v**2)/2) + m*h*g
+    return(fme)
 b = mechenergy(10,10,1)
 print(b)
 
