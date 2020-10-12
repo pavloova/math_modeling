7add3e3fc491a5f64af40c739553b594813fb9b0
pavloova <67763277+pavloova@users.noreply.github.com>
2020-10-12 18:01:16 +0300

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -3,8 +3,8 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt 
 t=np.linspace(0,9,100)
 def velocity(v,t):
-    v1=v+f/m*t
-    dvdt=(f-k*v1**2)/m
+   
+    dvdt=(f-k*v**2)/m
     
     return dvdt
 
