4da019fd1544cc063ff7bb52af4e9d30e33f4c45
pavloova <67763277+pavloova@users.noreply.github.com>
2020-10-12 17:20:05 +0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,9 +1,9 @@
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt 
-t=np.linspace(0,4,1000)
+t=np.linspace(0,365,1000)
 def invest_volume(invest,t):
-    dinvestdt=-k*invest0
+    dinvestdt=-k*invest
     return dinvestdt
 invest0=1000
 k=0.008
@@ -11,5 +11,3 @@ vol=odeint(invest_volume,invest0,t)
 plt.plot(t,vol[:,0],label='Investments') 
 plt.legend()
 plt.show()
-
-
