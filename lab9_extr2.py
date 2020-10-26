72e01110bbd13bc2aada02f287582682df718d6d
pavloova <67763277+pavloova@users.noreply.github.com>
2020-10-26 18:33:58 +0300

    Update lab9_extr2.py

--- lab9_extr2.py
+++ lab9_extr2.py
@@ -2,18 +2,19 @@
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
-t=np.linspace(0,10,100)
-def velocity(v,t):
-    dvdt=(G*m/(R+h)**2)*t
+h0=200000
+r=np.linspace(0,h0,100)
+def velocity(v,r):
+    dvdt=G*m/(v*(R+h0-r)**2)
     return dvdt
-v0=0
-h=20000
+v0=0.01
+
 G=6.67*10**(-11)
 m=5.97*10**24
 R=6400000
-vel = odeint(velocity, v0, t)
+vel = odeint(velocity, v0, r)
 
-plt.plot(t,vel[:,0],label='velocity')
+plt.plot(r,vel[:,0],label='velocity')
 plt.grid() 
 plt.legend()
 
